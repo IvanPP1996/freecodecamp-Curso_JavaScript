4154834966fa043fad7c094a35ab507cf7374973
Ivan Patiño <ivanrodrigopatinoplaza.1996@gmail.com>
2026-08-19 16:54:09 -0500

    (19/08/2026) Conceptos basicos

--- _3_conceptos_basicos.js
+++ _3_conceptos_basicos.js
@@ -25,3 +25,36 @@ console.log(b); // VALOR
 
 a = 10;
 console.log(a); // VALOR */
+
+// Asignar valor varaible a otra -----------------------------------------------------------------------
+
+/* var a = 5;
+var b = a;
+var c;
+
+c = b;
+
+console.log(a);
+console.log(b);
+console.log(c); */
+
+// Varaible inicializada ------------------------------------------------------------------------------
+
+/* var x = 9;
+var miIdioma = "Español"; */
+
+// Varaible no inicializada ---------------------------------------------------------------------------
+
+/* var a;
+var b;
+
+console.log(a); // UNDEFINED
+a =56;
+console.log(a); // VALOR */
+
+// Mayúsculas y minúsculas ---------------------------------------------------------------------------
+
+/* var miVariable = 5;
+console.log(MiVariable); // ReferenceError ReferenceError: MiVariable is not defined
+console.log(MiVARIABLE); // ReferenceError ReferenceError: MiVARIABLE is not defined
+console.log(mivariable); // ReferenceError ReferenceError: mivariable is not defined */
