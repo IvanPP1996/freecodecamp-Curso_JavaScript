1ffe0043f8b4d483314f960bcf82a8438b3cf71e
Ivan Patiño Plaza <ivanrodrigopatinoplaza.1996@gmail.com>
2026-08-18 19:17:15 -0500

    (18/08/2026) Conceptos_basicos

---
+++ _2_comentarios.js
@@ -0,0 +1,11 @@
+// comentario
+
+/* 
+Este es un comentario
+de varias líneas
+*/
+
+/* 
+Para comentar todo rápido selecciona el texto o código y presiona Shift + Alt + A (se comenta todo)
+Para comentar todo rápido selecciona el texto o código y presiona Ctrl + } (se comenta línea por línea)
+*/
---
+++ _3_conceptos_basicos.js
@@ -0,0 +1,27 @@
+/*
+Tipos de datos:
+undefined, null, boolean, string, symbol, number y object
+--------------------------------------------------------------------------------------------------------
+*/
+
+/* var numero = "freeCodeCamp";
+console.log(numero);
+
+numero = 16;
+console.log("Variable usando var: ",numero);
+
+let numeral = 67;
+console.log("Variable usando let: ", numeral);
+
+const constante = 3.1416;
+console.log("Cosntante: ", constante); */
+
+// Operador asignación ---------------------------------------------------------------------------------
+
+/* var a;
+var b = 2;
+console.log(a); // UNDEFINED
+console.log(b); // VALOR
+
+a = 10;
+console.log(a); // VALOR */
