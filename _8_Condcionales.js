23d874dd0aa5a49a7fe4e42280758298883c35ee
Ivan Patiño <ivanrodrigopatinoplaza.1996@gmail.com>
2026-08-26 15:15:29 -0500

    (26/08/2026) Condicionales

---
+++ _8_Condcionales.js
@@ -0,0 +1,54 @@
+// condicional if ------------------------------------------------------
+
+/* var x = 5;
+
+if (x > 8) {
+    console.log("La condición es verdadera");
+}else {
+    console.log("la condición es falsa");
+} */
+
+// condicional if else if -----------------------------------------------
+
+/* function clasificarvalor (valor) {
+    if (valor % 2 === 0){
+        console.log("Divisible entre 2");
+    } else if (valor % 3 === 0) {
+        console.log("Divisible entre 3");
+    } else {
+        console.log("No es divisible entre las opciones");
+    };
+};
+
+clasificarvalor(34.5); */
+
+// condicionales orden logico --------------------------------------------
+
+/* function clasificarValor(valor) {
+  if (valor < 5) {
+    console.log("Menor que 5");
+} else if (valor < 10) {
+    console.log("Menor que 10");
+} else {
+    console.log("El valor es igual o mayor");
+}
+}
+
+clasificarValor(15); */
+
+/* function interpretarIMC (indice) {
+    if (indice < 18.5) {
+        console.log("Bajo peso");
+    } else if (indice <= 24.9) {
+        console.log("normal");
+    } else if (indice <= 29.9) {
+        console.log("Sobrepeso");
+    } else{
+        console.log("Obeso");
+    };
+};
+
+interpretarIMC(17.8);
+interpretarIMC(22.2);
+interpretarIMC(28.2);
+interpretarIMC(34.2); */
