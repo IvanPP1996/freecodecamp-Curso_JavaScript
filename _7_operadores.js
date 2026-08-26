afe7695ebf0e12ee0fa0c0d749aa0e69dff0bdb4
Ivan Patiño <ivanrodrigopatinoplaza.1996@gmail.com>
2026-08-26 15:15:05 -0500

    (26/08/2026) Operadores

---
+++ _7_operadores.js
@@ -0,0 +1,101 @@
+// true y false ------------------------------------------------------------
+
+/* console.log(true);
+console.log(false); */
+
+// operador igualdad -------------------------------------------------------
+
+/* console.log(5 == 5);
+console.log(5 == 6);
+console.log("hola" == "Hola"); */
+
+// operador estricta -------------------------------------------------------
+
+/* console.log(9 === 9);
+console.log(9 === "9"); */
+
+/* var a = 5;
+var b = 5;
+
+console.log(a == b);
+console.log(a === b);
+
+b = 8;
+
+console.log(a == b);
+console.log(a === b);
+
+b = "5";
+
+console.log(a == b);
+console.log(a === b); */
+
+// operador desigualdad ----------------------------------------------------
+
+/* console.log(9 != 6);
+console.log(9 != 9);
+console.log("Java" != "Java");
+console.log("Java" != "JavaScript"); */
+
+// operador desigualdad estricta --------------------------------------------
+
+/* console.log(9 !== "9"); */
+
+// operador mayor que -------------------------------------------------------
+
+/* console.log(6 > 5);
+console.log(3 > 10);
+console.log("B" > "A"); // true
+console.log("ACB" > "ABC"); // true
+console.log("Mundo" > "Hola"); // true */
+
+// operador mayor o igual que -----------------------------------------------
+
+/* console.log(5 >= 5);
+console.log(5 >= 2);
+console.log(1 >= 5); */
+
+// operador menor que -------------------------------------------------------
+
+/* console.log(5 < 6);
+console.log(10 < 6);
+console.log("A" < "B"); */
+
+// operador menor o igual que -----------------------------------------------
+
+/* console.log(5 <= 5);
+console.log(5 <= 2);
+console.log(2 <= 5); */
+
+// operador logico and ------------------------------------------------------
+
+/* console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+var a = 8;
+console.log(a > 5 && a < 10);
+console.log(a > 5 && a < 7); */
+
+// operador logico or -------------------------------------------------------
+
+/* console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+var a = 8;
+console.log(a > 5 || a < 10);
+console.log(a > 5 || a < 7);
+console.log(a > 9 || a < 7); */
+
+// operador logico not ------------------------------------------------------
+
+/* console.log(!true);
+console.log(!false);
+
+var a = 8;
+
+console.log(!(a > 5));
+console.log(!(a < 5)); */
